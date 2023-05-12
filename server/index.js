f7b0e7037e3f4cd1b489c1ab68467cacad3d1c26
Mason Hirst <mhirstdev@gmail.com>
2023-05-12 10:13:10 -0600

    switched to only socket communication, now the deployed app doesn't have issues

--- server/index.js
+++ server/index.js
@@ -44,6 +44,7 @@ wss.on('connection', function connection(ws, req) {
   ws.on('message', function message(data, isBinary) {
     const message = JSON.parse(data).message
     console.log('received: %s', message)
+    console.log('clients size in onMessage: ', wss.clients.size)
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify({ message }))
@@ -64,7 +65,7 @@ wss.on('connection', function connection(ws, req) {
         client.ping()
       }
     })
-  }, 3000)
+  }, 2000)
 })
 
 //! Endpoints
